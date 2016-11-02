0c8c4ae1fe1c0ec008978279abe5146cce55e75a
alimariemac <aliciamaccara@gmail.com>
2016-11-02 19:34:58 +0000

    edited contacts table

--- db/migrate/20161102185537_creat.rb
+++ db/migrate/20161102185537_creat.rb
@@ -1,10 +1,10 @@
-class Creat < ActiveRecord::Migration
+class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
       t.string :email
       t.text :comments
-
       t.timestamps
+    end
   end
 end
