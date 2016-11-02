ea786e1d1991430d375f500ff3111a389174d27f
alimariemac <aliciamaccara@gmail.com>
2016-11-02 19:46:40 +0000

    recreated table

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102185537) do
+ActiveRecord::Schema.define(version: 20161102194327) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "comments"
+  end
 
 end
